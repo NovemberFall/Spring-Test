1fa7aeb94b3f34f3bf220dd7ef23ed7faecf2f75
TomZheng <1219.zheng@gmail.com>
2022-11-24 18:12:50 -0800

    JPA testing

--- Testing/src/test/java/com/example/Testing/customer/CustomerRepositoryTest.java
+++ Testing/src/test/java/com/example/Testing/customer/CustomerRepositoryTest.java
@@ -1,5 +1,9 @@
 package com.example.Testing.customer;
 
+import java.util.Optional;
+import java.util.UUID;
+import javax.swing.text.html.Option;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,5 +19,54 @@ class CustomerRepositoryTest {
         // Given
         // When
         // Then
+    }
+
+    // Failed this test
+    @Test
+    void itShouldSaveCustomerFail() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", "0000");
+
+        // When
+        underTest.save(customer);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+//        assertThat(optionalCustomer).isNotPresent();
+//        assertThat(optionalCustomer).isPresent();
+
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+                    assertThat(c.getId()).isEqualTo(id);
+                    assertThat(c.getName()).isEqualTo("Abel");
+                    assertThat(c.getPhoneNumber()).isEqualTo("1111");
+                });
+    }
+
+
+    // Failed this test
+    @Test
+    void itShouldSaveCustomer() {
+        // Given
+        UUID id = UUID.randomUUID();
+        Customer customer = new Customer(id, "Abel", "0000");
+
+        // When
+        underTest.save(customer);
+
+        // Then
+        Optional<Customer> optionalCustomer = underTest.findById(id);
+//        assertThat(optionalCustomer).isNotPresent();
+//        assertThat(optionalCustomer).isPresent();
+
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+                    assertThat(c.getId()).isEqualTo(id);
+                    assertThat(c.getName()).isEqualTo("Abel");
+                    assertThat(c.getPhoneNumber()).isEqualTo("0000");
+                });
     }
 }
